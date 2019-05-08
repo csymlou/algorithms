1f75e78e4093fbbfd0f837cb6b061bc654682d39
liliqing <liliqing@meituan.com>
2019-05-08 14:19:31 +0800

    2

--- python/structure/za.py
+++ python/structure/za.py
@@ -47,6 +47,49 @@ def comb(a, n):
 
 
 
+"""
+01背包
+wi:vi  初始容量U
+v=f[i][u], 0<=u<=U, f[0][U]=0, f[i][0]=0
+
+f[i][u] = max{f[i-1][u], f[i-1][u-wi]+vi}  st. wi<=u
+
+"""
+def bag01(stones, U):
+    N = len(stones)
+    dp = [[0] * (U + 1) for _ in range(N)] # dp[N][U+1]
+    for i in range(N):
+        w = stones[i][0] # 第i个石头的重量
+        v = stones[i][1] # 第i个石头的价值
+        for u in range(1, U + 1):
+            if u >= w:
+                dp[i][u] = max(dp[i-1][u], dp[i-1][u-w]+v) if i > 0 else v
+            else:
+                dp[i][u] = dp[i-1][u] if i > 0 else 0
+    return dp[-1][-1]
+
+
+"""
+完全背包
+
+f[i][u] = max{ f[i-1][u-k*wi] + k*vi }, st. 0 <= k*wi <= u
+
+"""
+def bag_full(stones, U):
+    N = len(stones)
+    dp = [[0] * (U + 1) for _ in range(N)] # dp[N][U+1]
+    for i in range(N):
+        w = stones[i][0] # 第i个石头的重量
+        v = stones[i][1] # 第i个石头的价值
+        for u in range(1, U+1):
+            k = 0
+            while k * w <= u:
+                dp[i][u] = max(dp[i][u], dp[i-1][u-k*w]+k*v if i > 0 else k*v)
+                k += 1
+    return dp[-1][-1]
+
+
+
 if __name__ == '__main__':
     a = [1,2,3]
     perm(a, 0, 2)
